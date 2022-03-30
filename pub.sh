bb10291f432b6a16e6a1e35561b6b20e7ffd32a2
aisling-krewer <aisling.krewer@shopify.com>
2022-03-30 16:56:40 +0100

    if loops to use other customisations

--- pub.sh
+++ pub.sh
@@ -1,10 +1,16 @@
-#! /usr/bin/env zsh 
+#! /usr/bin/env zsh
 source ~/.zshrc
 
 echo "Enter subdomain of store you're working on"
 read subdomain
 echo "What are you doing?"
 read task
+echo "What type of customisation are you working on?"
+echo "1 = Theme"
+echo "2 = Script"
+echo "3 = Order"
+echo "4 = Notification"
+read type
 echo "Enter theme ID of theme that you're working on"
 read themeid
 echo "Enter themekit password"
@@ -17,15 +23,19 @@ cd ~/Documents/sample-merchant-repo
 
 git checkout master && git pull origin master
 git checkout -b ${subdomain}-${task}-init
+
+if [[ $type -eq 1 ]]
+then
 mkdir -p ${subdomain}/${themeid}
 cd ${subdomain}/${themeid}
+echo 'option 1'
 tee -a config.yml << CON
 development:
   password:  ${themekit}
   theme_id:  ${themeid}
   store:  ${subdomain}.myshopify.com
   timeout: 30s
-  ignore_files: 
+  ignore_files:
       - "config/settings_data.json"
       - "config/settings.html"
       - "*.png"
@@ -39,6 +49,26 @@ CON
 code .
 
 theme download
+
+elif [[ $type -eq 2 ]]
+then
+  mkdir -p ${subdomain}/scripts
+  cd ${subdomain}/scripts
+  echo 'option 2'
+elif [[ $type -eq 3 ]]
+then
+  mkdir -p ${subdomain}/orders
+  cd ${subdomain}/orders
+  echo 'option 3'
+elif [[ $type -eq 4 ]]
+then
+  mkdir -p ${subdomain}/notifications
+  cd ${subdomain}/notifications
+  echo 'option 4'
+else
+  echo "Unexpected input" $type
+fi
+
 git add .
 git commit -m "${subdomain} ${task} init"
 git push origin ${subdomain}-${task}-init
